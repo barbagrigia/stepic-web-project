1a41cd44daced4d5a1e1461b6353b2b1fb30a18a
vtruhin <vvt@narod.ru>
2016-05-17 19:45:59 +0400

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,19 +1,19 @@
-from __future__ import unicode_literals
-
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
 
 class Question(models.Model):
-	title = models.CharField(max_length=255)
-	text = models.TextField()
-	added_ad = models.DateTimeField('Created', auto_now=True)
-	rating = models.IntegerField(default=0)
-	author = models.CharField(max_length=255)
-	likes = models.ManyToManyField(User)
+   title = models.CharField(max_length=255)
+   text = models.TextField()
+   added_at = models.DateTimeField(auto_now=True)
+   rating = models.IntegerField(default=0)
+   author = models.ForeignKey(User, related_name='question_author')
+   likes = models.ManyToManyField(User, related_name='question_likes')
+
 
 class Answer(models.Model):
-	text = models.TextField()
-	added_ad = models.DateTimeField('Created', auto_now=True)
-	author = models.CharField(max_length=255)
-	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+   text = models.TextField()
+   added_at = models.DateTimeField(auto_now=True)
+   question = models.ForeignKey(Question)
+   author = models.ForeignKey(User, related_name='answer_author')
