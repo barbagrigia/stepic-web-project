030c6de0972f60849e1afc60166b0e8f339e2319
vtruhin <vvt@narod.ru>
2016-05-17 18:20:14 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,9 +9,9 @@ sudo /etc/init.d/gunicorn restart
 
 #database conf
 sudo /etc/init.d/mysql restart
+mysql -uroot -e "DROP DATABASE ASK"
 mysql -uroot -e "CREATE DATABASE ASK"
 mysql -uroot -e "CREATE USER 'sa'@'localhost' IDENTIFIED BY 'sa'"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON ASK.* TO 'sa'@'localhost'"
-
 
 python /home/box/web/ask/manage.py syncdb
