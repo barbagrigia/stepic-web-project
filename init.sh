3fca4409fd96111e36c140e89fafa204a5f51fbc
vtruhin <vvt@narod.ru>
2016-05-17 16:03:18 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,5 +4,13 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 #gunicorn conf
-sudo ln -s /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/ask
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
+
+#database conf
+mysql -uroot -e "CREATE DATABASE ASK"
+mysql -uroot -e "CREATE USER 'sa'@'localhost' IDENTIFIED BY 'sa'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON ASK.* TO 'sa'@'localhost'"
+
+
+python /home/box/web/ask/manage.py syncdb
