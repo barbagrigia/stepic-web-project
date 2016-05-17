1d325507a7a058e96bb1343b7329e3962f807353
vtruhin <vvt@narod.ru>
2016-05-18 00:34:32 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,15 +1,19 @@
 #nginx conf
 sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 #gunicorn conf
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/ask
+sudo rm /etc/gunicorn.d/test
+sudo rm /etc/gunicorn.d/ask
+#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
 
 #database conf
-sudo /etc/init.d/mysql restart
-#mysql -uroot -e "DROP DATABASE ASK"
+#sudo /etc/init.d/mysql restart
+mysql -uroot -e "DROP DATABASE ASK"
 mysql -uroot -e "CREATE DATABASE ASK"
 mysql -uroot -e "CREATE USER 'sa'@'localhost' IDENTIFIED BY 'sa'"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON ASK.* TO 'sa'@'localhost'"
