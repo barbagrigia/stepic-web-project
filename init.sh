c48462ff1f3b5f623fbbf00eb55bb8d85ad1708e
vtruhin <vvt@narod.ru>
2016-05-17 04:08:22 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #nginx conf
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
