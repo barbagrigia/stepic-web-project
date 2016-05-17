9746e96d06e3064c9bd53a8821319b172c2d692c
vtruhin <vvt@narod.ru>
2016-05-17 19:42:59 +0400

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,16 +1,13 @@
-from django.conf.urls import url
-from django.contrib import admin
-from qa.views import test, basetemp, questpage, askform, newanswer, signupform, loginform, logoutPage
+from django.conf.urls import patterns, include, url
 
-urlpatterns = [
-	url(r'^$', basetemp, {'url':'/?page=','order':'-added_ad'}),
-	url(r'^login/.*$', loginform),
-	url(r'^logout/.*$', logoutPage),
-	url(r'^signup/.*$', signupform),
-	url(r'^question/(?P<slug>\d+)/$', questpage),
-	url(r'^question/$', askform),
-	url(r'^ask/.*$', askform),
-	url(r'^popular/.*$', basetemp, {'url':'/popular/?page=','order':'-likes'}),
-	url(r'^new/.*$', test),
-	url(r'^answer/.*$', newanswer),
-]
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'ask.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^', include('qa.urls')),
+    #url(r'^admin/', include(admin.site.urls)),
+)
