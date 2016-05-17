35452a694c51b947fcaec1e5b6c320598bec9be6
vtruhin <vvt@narod.ru>
2016-05-17 16:17:41 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,6 +8,7 @@ sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
 
 #database conf
+sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE DATABASE ASK"
 mysql -uroot -e "CREATE USER 'sa'@'localhost' IDENTIFIED BY 'sa'"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON ASK.* TO 'sa'@'localhost'"
