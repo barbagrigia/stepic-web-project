884e6df109853655a4c7eb38e0fe49bcf40d02a5
duk2000 <92007210+duk2000@users.noreply.github.com>
2021-10-06 13:51:48 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ sudo /etc/init.d/nginx restart
 #gunicorn conf
 sudo rm /etc/gunicorn.d/test
 sudo rm /etc/gunicorn.d/ask
-#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
 
